6ba21899085ac838f6e9f7fa6bbb33b94ec44c76
biz-haradah <h.harada@bizrefine.co.jp>
2021-05-18 17:04:10 +0900

    不要な空白スペースの削除

--- bizcalendar/src/main/java/jp/co/bizrefine/util/JwtAuthProviderUtil.java
+++ bizcalendar/src/main/java/jp/co/bizrefine/util/JwtAuthProviderUtil.java
@@ -7,9 +7,9 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 public class JwtAuthProviderUtil {
 
 	public static void verifyRequestToken(final String reqToken, final String secretKey) {
-		JWT.require(Algorithm.HMAC256(secretKey)).build().verify(reqToken);			
+		JWT.require(Algorithm.HMAC256(secretKey)).build().verify(reqToken);
 	}
-	
+
 	public static DecodedJWT decodeRequestToken(final String reqToken) {
 		return JWT.decode(reqToken);
 	}
--- bizcalendar/src/test/java/jp/co/bizrefine/util/JwtAuthProviderUtilTests.java
+++ bizcalendar/src/test/java/jp/co/bizrefine/util/JwtAuthProviderUtilTests.java
@@ -15,14 +15,14 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JwtAuthProviderUtilTests {
-	
+
 	@Test
 	public void testVerifyRequestToken() {
 		final String testMethodName = "testVerifyRequestToken";
 
 		final String reqToken = "";
 		final String secretKey = "";
-		
+
 		System.out.println("■ ■ ■ start: " + testMethodName + " ■ ■ ■");
 		try {
 			JwtAuthProviderUtil.verifyRequestToken(reqToken, secretKey);
@@ -35,11 +35,11 @@ public class JwtAuthProviderUtilTests {
 	@Test
 	public void testDecodeRequestToken() {
 		final String testMethodName = "testDecodeRequestToken";
-		
+
 		final String reqToken = "";
 		final Long testUserId = 0L;
 		final String testUserName = "";
-		
+
 		System.out.println("■ ■ ■ start: " + testMethodName + " ■ ■ ■");
 		DecodedJWT decodedJWT = null;
 		try {
@@ -47,11 +47,11 @@ public class JwtAuthProviderUtilTests {
 		} catch (JWTDecodeException jWTDecodeException) {
 			Assert.fail();
 		}
-		
+
 		Assert.assertNotNull(decodedJWT);
 		Assert.assertEquals(testUserId, decodedJWT.getClaim("user_id").asLong());
 		Assert.assertEquals(testUserName, decodedJWT.getClaim("name").toString().replaceAll("\"", ""));
-		
+
 		System.out.println("■ ■ ■ end  : " + testMethodName + " ■ ■ ■");
 	}
 }
